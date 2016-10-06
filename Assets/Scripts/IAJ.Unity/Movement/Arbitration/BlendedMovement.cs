1714d0d181968a25437e1f1d3c0584cdd54b482a
Henrique Moisés Fernandes <henriqemoises@gmail.com>
2016-10-06 23:55:51 +0100

    Class CharacterAvoidance implemented

--- Assets/Scripts/IAJ.Unity/Movement/Arbitration/BlendedMovement.cs
+++ Assets/Scripts/IAJ.Unity/Movement/Arbitration/BlendedMovement.cs
@@ -19,6 +19,7 @@ namespace Assets.Scripts.IAJ.Unity.Movement.Arbitration
             this.Movement = movement;
             this.Weight = weight;
         }
+
     }
 
     public class BlendedMovement : DynamicMovement.DynamicMovement
@@ -48,25 +49,26 @@ namespace Assets.Scripts.IAJ.Unity.Movement.Arbitration
             foreach (MovementWithWeight movementWithWeight in this.Movements)
             {
                 movementWithWeight.Movement.Character = this.Character;
-                
+
                 tempOutput = movementWithWeight.Movement.GetMovement();
                 if (tempOutput.SquareMagnitude() > 0)
                 {
                     finalOutput.linear += tempOutput.linear * movementWithWeight.Weight;
                     finalOutput.angular += tempOutput.angular * movementWithWeight.Weight;
-                    totalWeight += movementWithWeight.Weight;    
+                    totalWeight += movementWithWeight.Weight;
                 }
             }
 
             if (totalWeight > 0)
             {
                 //in case the totalWeight is not 1, we need to normalize
-                float normalizationFactor = 1.0f/totalWeight;
+                float normalizationFactor = 1.0f / totalWeight;
                 finalOutput.linear *= normalizationFactor;
                 finalOutput.angular *= normalizationFactor;
             }
 
             return finalOutput;
+
         }
     }
 }
---
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/CharacterAvoidance.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+
+    
+    class CharacterAvoidance
+    {
+        public DynamicCharacter character { get; set; }
+        public float  maxAcceleration { get; set; }
+        public float collisionRadius { get; set; }
+        public DynamicCharacter target { get; set; }
+        public float maxTimeLookahead { get; set; }
+
+        public MovementOutput getMovement() {
+            MovementOutput output = new MovementOutput();
+            KinematicData dataCharacter = character.KinematicData;
+            KinematicData dataTarget = target.KinematicData;
+            UnityEngine.Vector3 futureDeltaPos;
+            float futureDistance;
+            float timeToClosest;
+            UnityEngine.Vector3 deltaPos = dataTarget.position - dataCharacter.position;
+            UnityEngine.Vector3 deltaVel = dataTarget.velocity - dataCharacter.velocity;
+            float deltaSpeed = deltaVel.magnitude;
+
+            if (deltaSpeed == 0)
+                return output;
+
+            timeToClosest = -(Vector3.Dot(deltaPos, deltaVel) / (deltaSpeed * deltaSpeed));
+
+            if (timeToClosest > maxTimeLookahead)
+                return output;
+
+            futureDeltaPos = deltaPos + deltaVel * timeToClosest;
+            futureDistance = futureDeltaPos.magnitude;
+
+            if (futureDistance > 2 * collisionRadius)
+                return new MovementOutput();
+
+            if (futureDistance <= 0 || deltaPos.magnitude < 2 * collisionRadius)
+                output.linear = dataCharacter.position - dataTarget.position;
+            else
+                output.linear = futureDeltaPos * -1;
+
+            output.linear = output.linear.normalized * maxAcceleration;
+            return output;
+                }
+
+    }
+}
